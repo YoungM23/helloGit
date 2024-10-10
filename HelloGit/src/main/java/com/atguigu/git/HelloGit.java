3a3e54395d413061d5b752b19c51524d7c42c635
ZhangQingHui <qinghuizhang23@qq.com>
2024-10-10 10:54:29 +0800

    凭证提交测试

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -7,7 +7,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!");
         System.out.println("创建分支");
-        System.out.println("在线更新1");
+        System.out.println("凭证提交测试");
         
     }
 }
